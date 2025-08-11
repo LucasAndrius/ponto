678895ab4d7963579e04875c06ce1c621fc64947
LucasAndrius <devlucasandrius@gmail.com>
2025-08-10 23:24:36 -0300

    feat: atualiza responsividade da pagina home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,21 @@ import Image from "next/image";
 import CardHour from "./Components/CardHour/CardHour";
 import BotaoPonto from "./Components/ButtonHour/ButtonHour";
 
-
 export default function Home() {
   return (
     <main>
-      <div className="grid grid-cols-6 min-h-screen">
-        <aside className="bg-yellow-100 text-black text-center py-6">
-          <h2>últimos registros</h2>
-          <div className="my-6">
+      <div className="min-h-screen grid grid-cols-6 gap-4 p-4
+                      md:grid-cols-6">
+        {/* Aside fica full width no mobile, col-span-1 no desktop */}
+        <aside className="bg-yellow-100 text-black text-center py-6 col-span-6 md:col-span-1 rounded-md shadow-md">
+          <h2 className="text-lg font-semibold mb-4">Últimos registros</h2>
+          <div className="my-6 max-h-[60vh] overflow-auto">
             <CardHour />
           </div>
         </aside>
-        <div className="col-span-5 flex items-center justify-center">
+
+        {/* Botão ocupa toda a largura no mobile, col-span-5 no desktop */}
+        <div className="col-span-6 md:col-span-5 flex items-center justify-center">
           <BotaoPonto />
         </div>
       </div>
